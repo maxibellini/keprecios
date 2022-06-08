2657963d6488905620cc1e042e47ae6c3fc653be
Maximiliano Bellini <maxi.a.bellini@gmail.com>
2022-06-07 21:12:40 -0300

    Esqueleto del proyecto en symfony

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    /**
+     * @Route("/kp-admin", name="admin")
+     */
+    public function index(): Response
+    {
+        return parent::index();
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('EasyAdmin');
+    }
+
+    public function configureCrud(): Crud
+    {
+        return Crud::new();
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToCrud('User', 'fas fa-folder-open', User::class);
+    }
+}
